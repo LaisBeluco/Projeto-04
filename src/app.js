bde7265c2f645a26772f4b0aeb7faf0f24b44561
Lais B <laisbelucom@gmail.com>
2022-09-06 21:44:42 -0300

    Implementação do delete por id

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-import  express from "express";
+import express from "express";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 const usuarios = [
   {
-    id: 123,
+    id: 1,
     name: "João Silva",
     cpf: 90841540020,
-    "birthDate": "01 / 01 / 2000",
+    birthDate: "01 / 01 / 2000",
     email: "joao.silva@compasso.com",
   },
 ];
@@ -22,17 +22,31 @@ app.get("/api/v1/user", (req, res) => {
   res.status(200).json(usuarios);
 });
 
-app.post("/api/v1/user", (req,res) =>{
+app.get("/api/v1/user/:id", (req, res) => {
+  let index = pesquisarUsuario(req.params.id);
+  res.json(usuarios[index]);
+});
+
+app.post("/api/v1/user", (req, res) => {
   usuarios.push(req.body);
-  res.status(201).send("Usuário cadastrado com sucesso!")
-})
+  res.status(201).send("Usuário cadastrado com sucesso!");
+});
 
-app.put("/api/v1/user/:id", (req, res) =>{
+app.put("/api/v1/user/:id", (req, res) => {
+  let index = pesquisarUsuario(req.params.id);
+  usuarios[index].name = req.body.name;
+  res.json(usuarios);
+});
 
-})
+app.delete("/api/v1/user/:id", (req, res) => {
+  let {id} = req.params;
+  let index = pesquisarUsuario(id);
+  usuarios.splice(index, 1);
+  res.send(`Usuário ${id} removido com sucesso!`);
+});
 
-function pesquisarUsuario(id){
-  return usuarios.findIndex(usuario => usuario.id == id )
+function pesquisarUsuario(id) {
+  return usuarios.findIndex((usuario) => usuario.id == id);
 }
 
 export default app;
